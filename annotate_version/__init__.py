0d347dcbfd9c0c95c286934fea6d2abd4a9c713c
Lars Viklund <zao@zao.se>
2024-01-04 06:45:48 +0100

    feat: use pefile and dataapi

--- annotate_version/__init__.py
+++ annotate_version/__init__.py
@@ -1,69 +1,102 @@
-__version__ = '0.1.0'
+__version__ = "0.2.0"
 
 import codecs
+import io
 import logging
 from pathlib import Path
+import pathlib
+import re
+import sys
+import pefile
 import ndjson
 import os
 from plumbum import ProcessExecutionError, local
 import requests
 import zstandard
 
+CHANGEAPI_URL = os.getenv("CHANGEAPI_URL")
+CHANGEAPI_MOLLY_GUARD = os.getenv("CHANGEAPI_MOLLY_GUARD")
+DATAAPI_URL = os.getenv("DATAAPI_URL")
+
+
 def main():
     logging.basicConfig(format="%(asctime)s | %(message)s", level=logging.INFO)
     LOG = logging.getLogger()
-    root_dir = Path(os.getenv('ROOT_DIR'))
-    index_dir = root_dir / 'index'
-    data_dir = root_dir / 'data'
-    builds_url = f'{os.getenv("API_URL")}/builds/public'
-    molly_guard = os.getenv('MOLLY_GUARD')
+    builds_url = f"{CHANGEAPI_URL}/builds/public"
 
     builds = requests.get(builds_url).json()
 
-    cat = local['cat']
-    grep = local['grep']
-    strings = local['strings']
-    zstdcat = local['zstdcat']
-
     for bid, build in builds.items():
-        if 'version' not in build or build['version'] is None:
+        if "version" not in build or build["version"] is None:
             depot = None
-            manifests = build['manifests']
-            for d in ['238962', '238961']:
+            manifests = build["manifests"]
+            for d in ["238962", "238961"]:
                 if d in manifests:
                     depot = d
                     break
             manifest = manifests[depot]
+            gid = manifest["gid"]
 
-            path = index_dir / f'{depot}/{manifest}-loose.ndjson.zst'
-            if path.exists():
-                LOG.info(path)
-                fh = path.open(mode='rb')
+            mf_url = f"{DATAAPI_URL}/idxz/{depot}/{gid}/loose"
+            resp = requests.get(mf_url)
+            if resp.status_code == 200:
+                LOG.info(mf_url)
                 dctx = zstandard.ZstdDecompressor()
-                with dctx.stream_reader(fh, closefd=True) as fh:    
-                    with codecs.getreader('UTF-8')(fh) as fh:
+                with dctx.stream_reader(io.BytesIO(resp.content), closefd=True) as fh:
+                    with codecs.getreader("UTF-8")(fh) as fh:
                         cands = []
                         reader = ndjson.reader(fh)
+                        exe_urls = []
                         for row in reader:
-                            if '.exe' in row["path"]:
+                            if row["path"].endswith('.exe'):
                                 hash = row["sha256"]
-                                if row["comp"]:
-                                    path = data_dir / f'{hash[:2]}/{hash}.bin.zst'
-                                    extractor = zstdcat[path]
+                                exe_url = f"{DATAAPI_URL}/cad/{hash}"
+                                exe_urls.append(exe_url)
+                                exe_resp = requests.get(exe_url)
+                                if exe_resp.status_code == 200:
+                                    try:
+                                        data = exe_resp.content
+                                        pe = pefile.PE(
+                                            data=data, fast_load=True
+                                        )
+                                    except pefile.PEFormatError as e:
+                                        p = pathlib.Path("C:/Temp") / f"{hash}.exe"
+                                        p.write_bytes(data)
+                                        LOG.info(row)
+                                        LOG.fatal(f"{e}: Bogus EXE: {p}")
+                                        raise
+                                    r = re.compile(b"(?:release )?tags/(\d[-\da-z. ]{4,40})")
+                                    img = data = pe.get_memory_mapped_image()
+                                    for section in pe.sections:
+                                        va = section.VirtualAddress
+                                        for it in r.finditer(
+                                            img, va, va + section.Misc_VirtualSize
+                                        ):
+                                            cands.append(it.group(1))
                                 else:
-                                    path = data_dir / f'{hash[:2]}/{hash}.bin'
-                                    extractor = cat[path]
-                                try:
-                                    pipe = extractor | strings['-d', '-n', '14'] | grep['release tags/']
-                                    cands.extend(pipe().splitlines())
-                                except ProcessExecutionError:
-                                    pass
+                                    LOG.warn(
+                                        f"missing {exe_url}, {exe_resp.status_code}; {row['path']}"
+                                    )
                         if len(cands):
-                            version = sorted(cands, key=len)[-1]
-                            version = version.partition('/')[2]
+                            cands = sorted(cands, key=len, reverse=True)
+                            version = cands[0]
+                            print(cands)
                             print(version)
-                            resp = requests.put(f'{builds_url}/{bid}/version', params={'version': version, 'molly_guard': molly_guard})
+                            resp = requests.put(
+                                f"{builds_url}/{bid}/version",
+                                params={
+                                    "version": version,
+                                    "molly_guard": CHANGEAPI_MOLLY_GUARD,
+                                },
+                            )
                             LOG.info(resp.status_code)
+                        else:
+                            LOG.error(
+                                f"no candidates found for {depot}/{gid}"
+                            )
+                            for url in exe_urls:
+                                LOG.error(url)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
